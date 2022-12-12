7bbe6a95d83d80d5366685767a25168104079877
Aniket Satish Mane <aniketsmd310@gmail.com>
2022-12-12 20:42:28 +0530

    Modified folder structure

---
+++ Backend/src/api/index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    teacher: require('./teacher'),
+    student: require('./student'),
+    admin: require('./admin'),
+    attendance:require('./attendance'),
+    college:require('./college'),
+}
