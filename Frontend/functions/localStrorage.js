15f6326f54bb0c17834722efc9fe34b8d67c5a84
Aniket Satish Mane <aniketsmd310@gmail.com>
2023-03-09 18:09:56 +0530

    resolved build problem

--- Frontend/functions/localStrorage.js
+++ Frontend/functions/localStrorage.js
@@ -5,8 +5,7 @@ export const setUserInfo = (data) => {
   localStorage.setItem("userInfo", JSON.stringify(jwt_decode(data)));
 };
 
-
 export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("userInfo");
-};
+}
