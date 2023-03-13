f65068c0d204ae90833cb0d89c99959818021dfe
Aniket Satish Mane <aniketsmd310@gmail.com>
2023-03-13 16:58:58 +0530

    API's for Teacher section completd

--- Backend/src/controllers/attendanceController.ts
+++ Backend/src/controllers/attendanceController.ts
@@ -7,11 +7,24 @@ import { hasExpired } from "../util/functions";
 import AttendanceModel from "../models/attendance";
 import StudentModel from "../models/student";
 import TeacherModel from "../models/teacher";
+import SubjectModel from "../models/subject";
+
+
+declare global {
+    interface Map<K, V> {
+        getOrElse(key: K, defaultValue: V): V;
+    }
+}
+
+Map.prototype.getOrElse = function <K, V>(key: K, defaultValue: V): V {
+    return this.has(key) ? this.get(key) : defaultValue;
+};
 
 
 export const setAttendance = async (req: Request, res: Response, next: NextFunction) => {
     const { classId, teacherId, subjectId } = req.query;
     const { ip, expiredAt, date, attendanceId } = req.body;
+
 
     try {
 
@@ -93,7 +106,7 @@ export const markAttendance = async (req: Request, res: Response, next: NextFunc
         }
 
         if (hasExpired(currentTime, subject[0].notifications.expiredAt)) {
-          
+
             throw createHttpError(406, "Time Expired!");
         }
 
@@ -103,7 +116,8 @@ export const markAttendance = async (req: Request, res: Response, next: NextFunc
 
 
         const status = await StudentModel.findByIdAndUpdate(
-            studentId,{ $set: { [`status.${subjectId}`]: true } },{ new: true, upsert: true }).select("status -_id");
+            studentId, { $set: { [`status.${subjectId}`]: true } }, { new: true, upsert: true }).select("status -_id");
+
 
         return res.status(201).json(status)
 
@@ -111,6 +125,8 @@ export const markAttendance = async (req: Request, res: Response, next: NextFunc
         next(err);
     }
 }
+
+
 
 export const getAttendanceInfoT = async (req: Request, res: Response, next: NextFunction) => {
     const { classId, teacherId, subjectId }: any = req.query;
@@ -126,7 +142,7 @@ export const getAttendanceInfoT = async (req: Request, res: Response, next: Next
             throw createHttpError(409, "Class does not exist!");
         }
 
-        const data: any =await ClassModel.aggregate([
+        const data: any = await ClassModel.aggregate([
             { $match: { _id: new mongoose.Types.ObjectId(classId) } },
             {
                 $project: {
@@ -142,7 +158,7 @@ export const getAttendanceInfoT = async (req: Request, res: Response, next: Next
                 }
             }])
 
-        return res.status(201).json({notifications:data[0].notifications, ip: req.ip });
+        return res.status(201).json({ notifications: data[0].notifications, ip: req.ip });
     } catch (err) {
         next(err);
     }
@@ -162,15 +178,101 @@ export const getAttendanceInfoS = async (req: Request, res: Response, next: Next
             throw createHttpError(409, "Class does not exist!");
         }
 
-        const data= await ClassModel.findById(classId, { notifications: 1, _id: 0 });
+        const data = await ClassModel.findById(classId, { notifications: 1, _id: 0 });
 
-        return res.status(201).json({ notifications:data?.notifications, status: isStudentExist.status, ip: req.ip });
+        return res.status(201).json({ notifications: data?.notifications, status: isStudentExist.status, ip: req.ip });
 
     } catch (err) {
         next(err)
     }
 }
 
+export const getAttendanceDetails = async (req: Request, res: Response, next: NextFunction) => {
 
+    const { classId, subjectId }: any = req.query;
+
+    try {
+
+
+        const isSubjectExist = await SubjectModel.findById(subjectId);
+
+        if (!isSubjectExist) {
+            throw createHttpError(409, "Subject does not exist!");
+        }
+
+        const isClassExist: any = await ClassModel.findById(classId, { classSubjects: 1, _id: 0 });
+
+        if (!isClassExist) {
+            throw createHttpError(409, "Subject does not exist!");
+        }
+
+        const attendanceId = isClassExist.classSubjects.get(subjectId).attendanceId;
+
+
+        const students = await StudentModel.find({ classId },
+            { status: 0, password: 0, role: 0, gender: 0, dob: 0, departmentId: 0, classId: 0 });
+
+        const { attendanceDetails }: any = await AttendanceModel.findById(attendanceId, { attendanceDetails: 1, _id: 0 });
+
+
+        const freqMap = new Map<string, number>();
+
+        for (let i = 0; i < attendanceDetails.length; i++) {
+            for (let j = 0; j < attendanceDetails[i].presentStudents.length; j++) {
+                freqMap.set(attendanceDetails[i].presentStudents[j], freqMap.getOrElse(attendanceDetails[i].presentStudents[j], 0) + 1)
+            }
+        }
+
+        return res.status(201).json({ students, attendanceDetails, map: Object.fromEntries(freqMap) })
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const absentStudents = async (req: Request, res: Response, next: NextFunction) => {
+    const { date, subjectId, classId }: any = req.query;
+    try {
+
+        const isSubjectExist = await SubjectModel.findById(subjectId);
+
+        if (!isSubjectExist) {
+            throw createHttpError(409, "Subject does not exist!");
+        }
+
+        const isClassExist: any = await ClassModel.findById(classId, { classSubjects: 1, _id: 0 });
+
+        if (!isClassExist) {
+            throw createHttpError(409, "Subject does not exist!");
+        }
+        const attendanceId = isClassExist.classSubjects.get(subjectId).attendanceId;
+
+        const response = await AttendanceModel.aggregate([
+            { $match: { _id: new mongoose.Types.ObjectId(attendanceId) } },
+            { $unwind: '$attendanceDetails' },
+            { $match: { 'attendanceDetails.date': date } },
+            { $project: { _id: 0, presentStudents: '$attendanceDetails.presentStudents' } }
+        ]);
+
+        const pStudents = new Set(response[0].presentStudents);
+
+        const students: any = await StudentModel.find({ classId }, { fname: 1, image: 1, regNo: 1, rollNo: 1 });
+        const absentStudents = [];
+
+        for (let i = 0; i < students.length; i++) {
+            if (!pStudents.has(students[i]._id + "")) {
+                absentStudents.push(students[i]);
+            }
+        }
+
+
+        return res.status(201).json({ absentStudents });
+
+
+
+    } catch (err) {
+        next(err);
+    }
+}
 
 
--- Backend/src/routes/attendanceRoutes.ts
+++ Backend/src/routes/attendanceRoutes.ts
@@ -5,11 +5,11 @@ import * as Auth from "../middlewares/authorization"
 const router = express.Router();
 
 router.put("/setAttendance",AttendanceController.setAttendance);
-
 router.put('/markAttendance',AttendanceController.markAttendance);
 
 router.get('/getInfoT',AttendanceController.getAttendanceInfoT);
-
 router.get('/getInfoS',AttendanceController.getAttendanceInfoS);
+router.get('/getDetails',AttendanceController.getAttendanceDetails);
+router.get('/absentStudents',AttendanceController.absentStudents);
 
 export default router;
