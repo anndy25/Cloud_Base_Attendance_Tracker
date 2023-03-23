3dead8207756317615e9380cdaa09fc28bca3547
Aniket Satish Mane <aniketsmd310@gmail.com>
2023-03-23 17:20:34 +0530

    final changes for scheduler

--- Backend/src/cron/scheduler.ts
+++ Backend/src/cron/scheduler.ts
@@ -2,27 +2,24 @@ import cron from "node-cron";
 import ClassModel from "../models/class";
 import StudentModel from "../models/student";
 
-function formatDateTime(date:Date):string {
-    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const dayOfWeek = daysOfWeek[date.getDay()];
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `${dayOfWeek} ${hours}.${minutes}`;
-  }
-
- const scheduler = async () => {
-    cron.schedule('*/1 * * * *', async () => {
-        try {
-
-            // console.log("1min")
-            // await ClassModel.updateMany({}, { $set: { notifications: [] } });
-            // await StudentModel.updateMany({}, { $set: { status: {} } });
-            console.log(`deleted old data in notifications ${formatDateTime(new Date())}`);
-
-        } catch (err) {
-            console.log(err);
-        }
-    })
+function formatDateTime(date: Date): string {
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  return `${dayOfWeek} ${hours}.${minutes}`;
 }
+
+const scheduler = async () => {
+  cron.schedule('30 18 * * *', async () => {
+    try {
+      await ClassModel.updateMany({}, { $set: { notifications: [] } });
+      await StudentModel.updateMany({}, { $set: { status: {} } });
+      console.log(`deleted old data in notifications ${formatDateTime(new Date())}`);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+};
 
 export default scheduler;
